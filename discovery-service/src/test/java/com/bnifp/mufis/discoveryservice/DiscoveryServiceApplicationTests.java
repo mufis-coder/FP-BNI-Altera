4dcec9244fd063422a8b861435ab27afc8ee4774
mufis-coder <muhamadfikrisunandar@gmail.com>
2021-12-21 21:39:57 +0700

    init discovery channel

---
+++ discovery-service/src/test/java/com/bnifp/mufis/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnifp.mufis.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
