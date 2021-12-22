69a82fc11b88b0ff9bbd3452f9fa3a03a0d78a52
mufis-coder <muhamadfikrisunandar@gmail.com>
2021-12-22 18:58:51 +0700

    add config server

---
+++ config-server/src/test/java/com/bnifp/mufis/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnifp.mufis.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
