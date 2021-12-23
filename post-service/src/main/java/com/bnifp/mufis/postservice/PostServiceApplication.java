5ba39646842a3756a16f6e9fc90a262f53a4a34a
mufis-coder <muhamadfikrisunandar@gmail.com>
2021-12-23 09:43:18 +0700

    add swagger

--- post-service/src/main/java/com/bnifp/mufis/postservice/PostServiceApplication.java
+++ post-service/src/main/java/com/bnifp/mufis/postservice/PostServiceApplication.java
@@ -7,10 +7,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableEurekaClient
 @SpringBootApplication()
+@EnableSwagger2
 public class PostServiceApplication {
 
 	public static void main(String[] args) {
