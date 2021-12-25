1a4db5ad8a9cab8c00e04bc60f87599f53b18ca5
mufis-coder <muhamadfikrisunandar@gmail.com>
2021-12-25 11:15:19 +0700

    log service

--- log-service/src/main/java/com/bnifp/mufis/logservice/repository/LogRepository.java
+++ log-service/src/main/java/com/bnifp/mufis/logservice/repository/LogRepository.java
@@ -1,9 +1,9 @@
 package com.bnifp.mufis.logservice.repository;
 
-import com.bnifp.mufis.logservice.model.Log;
+import com.bnifp.mufis.logservice.dto.input.LogInput;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LogRepository extends MongoRepository<Log, String> {
+public interface LogRepository extends MongoRepository<LogInput, String> {
 }
