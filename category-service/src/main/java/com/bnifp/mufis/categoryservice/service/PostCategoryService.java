b13a82f3293570d11634fbd79d8e4ac1f95111f5
mufis-coder <muhamadfikrisunandar@gmail.com>
2022-01-06 13:12:39 +0700

    add post fit categories

--- category-service/src/main/java/com/bnifp/mufis/categoryservice/service/PostCategoryService.java
+++ category-service/src/main/java/com/bnifp/mufis/categoryservice/service/PostCategoryService.java
@@ -1,10 +1,15 @@
 package com.bnifp.mufis.categoryservice.service;
 
 import com.bnifp.mufis.categoryservice.dto.input.PostCategoryInput;
+import com.bnifp.mufis.categoryservice.dto.output.CategoryOutput;
 import com.bnifp.mufis.categoryservice.exception.DataNotFoundException;
 import com.bnifp.mufis.categoryservice.exception.InputNullException;
+import com.bnifp.mufis.categoryservice.model.PostCategory;
+
+import java.util.List;
 
 public interface PostCategoryService {
     String addOne(PostCategoryInput input) throws InputNullException;
     String deleteOne(Long Id) throws DataNotFoundException;
+    List<PostCategory> getAll();
 }
